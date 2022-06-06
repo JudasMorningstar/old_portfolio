92089faba4c71a89ed9a90379ab89527efba6ebc
judasMorningstar <junade.dev@gmail.com>
2022-06-06 02:46:25 +0200

    social

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -6,13 +6,19 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
+          <a href='https://github.com/JudasMorningstar'>
             <BsGithub />
+          </a>
         </div>
         <div>
+          <a href='https://www.instagram.com/_halftoothbandit/'>
             <BsInstagram />
+          </a>
         </div>
         <div>
+          <a href='https://www.figma.com/@junade'>
             <FaFigma />
+          </a>
         </div>
     </div>
   )
