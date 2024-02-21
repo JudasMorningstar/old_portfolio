0e332e67840f67fd15f9dd922f5f2154d66a2db8
Junade Mchunu <86800021+JudasMorningstar@users.noreply.github.com>
2024-02-21 09:04:54 +0200

    Update Header.jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => (
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Graphic Desiger</p>
+          <p className="p-text">Graphic Designer</p>
           <p className="p-text">Full Stack Developer</p>
           <p className="p-text">Aspiring ML Engineer</p>
         </div>
